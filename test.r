d15bfaabe882c1217a0cad45b5c8dfd992f53e8c
francisco juarez <fjuarez@fi.uba.ar>
2023-06-22 23:49:16 -0300

    terminado ej1 y empezado ej2

--- test.r
+++ test.r
@@ -1,4 +1,4 @@
-x <- c(20, 4, 6, 8, 10, 101, 99, 70, 80, 50)
+x <- c(20, 4, 6, 8, 10, 101, 99, 70, 80, 50) #10
 
 
 estimar_mediana <- function(x) {
@@ -14,19 +14,13 @@ estimar_mediana <- function(x) {
     
     
     if (empirica_actual >= 0.5) {
-      if (empirica_minima < 0.5 | empirica_actual < empirica_minima) {
+      if (empirica_minima < 0.5 || empirica_actual < empirica_minima) {
         empirica_minima <- empirica_actual
-        mediana_minima <- x[i] ############ da 4,6, y 8 bastantes veces, ta bien?
-        
-        #if (mediana_minima <= 6) {
-        #  print(mediana_minima)
-        #  print(x)
-        #}
-        
+        mediana_minima <- x[i] #da 4,6, y 8 bastantes veces, ta bien? Parece q si
       }
     }
   }
-  return (mediana_minima)
+  return(mediana_minima)
 }
 
 intervalo_boot <- function(x, nivel) {
@@ -35,20 +29,54 @@ intervalo_boot <- function(x, nivel) {
   for (i in 1:B) {
     bootstrap_x <- sample(x, replace = TRUE)
     replicaciones_bootstrap[i] <- estimar_mediana(bootstrap_x)
-    
   }
   
+  alpha <- (1 - nivel) / 2
+  lower <- quantile(replicaciones_bootstrap, alpha)
+  upper <- quantile(replicaciones_bootstrap, 1 - alpha)
+
+  return(c(lower, upper))
+}
+
+#intervalo_boot(x,0.95)
+
+cubrimiento_empirico <- function(n) {
+  lambda <- 5  # Parámetro de la distribución exponencial
+  nivel <- 0.05
   
-  print(replicaciones_bootstrap)
+  # Definir la distribución exponencial
+  distribucion <- rexp(n, rate = lambda)
   
+  # Simulación de 1000 muestras aleatorias y cálculo de intervalos de confianza
+  num_simulaciones <- 1000
+  cubrimiento <- numeric(num_simulaciones)  # Almacenar los resultados de cubrimiento
+  longitudes <- numeric(num_simulaciones)  # Almacenar las longitudes de los intervalos
+  intervalos <- c()
   
-  cota_inf <- quantile(replicaciones_bootstrap, nivel/2)
-  cota_sup <- quantile(replicaciones_bootstrap, 1 - nivel/2)
+  for (i in 1:num_simulaciones) {
+    muestra_i <- sample(distribucion, n, replace = TRUE)
+    mediana_i <- estimar_mediana(muestra)
+    alpha <- (1 - nivel) / 2
+    lower <- quantile(muestra, alpha)
+    upper <- quantile(muestra, 1 - alpha)
+    
+    # Comprobamos si la verdadera mediana (1/lambda) está dentro del intervalo
+    cubrimiento[i] <- intervalo[1] <= (1/lambda) && intervalo[2] >= (1/lambda)
+    
+    # Calculamos la longitud del intervalo
+    longitudes[i] <- intervalo[2] - intervalo[1]
+  }
   
-  #P(cota_inf <= mediana_real <= cota_sup) >= 1-nivel
+  # Proporción de intervalos que contienen la verdadera mediana
+  proporcion_cubrimiento <- sum(cubrimiento) / num_simulaciones
   
+  # Longitud promedio de los intervalos
+  longitud_promedio <- mean(longitudes)
   
+  # Devolvemos la lista con los resultados
+  return(list(proporcion_cubrimiento = proporcion_cubrimiento, longitud_promedio = longitud_promedio))
 }
 
 
-intervalo_boot(x,0.95)
+
+
